bba48f2b68e9faefbe8b405e1b5b92760f87c3e3
Quadri Sheriff <quadrisheriff0@gmail.com>
2021-06-25 13:57:58 +0100

    new

---
+++ blog-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '01427054e35f1e638ad5e083c4591a30'),
+    },
+  },
+});
---
+++ blog-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b294fba2-716b-4d61-92ab-6cd58519140e'
+};
